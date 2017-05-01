4394a19ba90c78cb222436c5441c14576029856c
Anton Shvab <shwab.ay@gmail.com>
2017-05-02 00:19:28 +0300

    add test

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import genDiff from '../src/';
+
+const path = '__tests__/data/json/';
+
+test('empty configs', () => {
+  const fstConfig = `${path}empty.json`;
+  const sndConfig = `${path}empty.json`;
+  const result = '';
+  expect(genDiff(fstConfig, sndConfig)).toBe(result);
+});
+
+// test('fst empty, snd not empty', () => {});
+
+// test('fst not empty, snd empty', () => {});
+
+test('same', () => {
+  const fstConfig = `${path}fst.json`;
+  const sndConfig = `${path}reversedFst.json`;
+  const result = fs.readFileSync(`${path}reversedDiff.txt`, 'utf-8');
+  expect(genDiff(fstConfig, sndConfig)).toBe(result);
+});
+
+test('diff of flat configs', () => {
+  const fstConfig = `${path}fst.json`;
+  const sndConfig = `${path}snd.json`;
+  const result = fs.readFileSync(`${path}diff.txt`, 'utf-8');
+  expect(genDiff(fstConfig, sndConfig)).toBe(result);
+});
+
