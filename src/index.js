48d70884a1a75d3f2768e056e6677be7b83e5ba9
Anton Shvab <shwab.ay@gmail.com>
2017-05-02 16:10:07 +0300

    add ini parser

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parse = (configPath) => {
   const isEmptyConfig = fs.statSync(configPath).size === 0;
+  const configString = isEmptyConfig ? '{}' : fs.readFileSync(configPath, 'utf-8');
   const extname = path.parse(configPath).ext;
   if (extname === '.json') {
-    return isEmptyConfig ? {} : JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+    return JSON.parse(configString);
   }
   if (extname === '.yaml') {
-    return isEmptyConfig ? {} : yaml.safeLoad(fs.readFileSync(configPath, 'utf-8'));
+    return yaml.safeLoad(configString);
+  }
+  if (extname === '.ini') {
+    return ini.parse(configString);
   }
   return new Error(`${extname} extension is not supported!`);
 };
