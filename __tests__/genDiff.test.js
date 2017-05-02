03ecc3054b1f1fb12bbe67ce8c9319886f468338
Anton Shvab <shwab.ay@gmail.com>
2017-05-02 15:22:46 +0300

    add test suites

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,32 +3,41 @@ import path from 'path';
 import genDiff from '../src/';
 
 const dirJson = '__tests__/__fixtures__/json/';
+const dirYaml = '__tests__/__fixtures__/yaml/';
 
-test('empty configs', () => {
-  const fstPath = path.join(dirJson, 'empty.json');
-  const sndPath = fstPath;
-  const resultPath = path.join(dirJson, 'emptyDiff.txt');
-  const result = fs.readFileSync(resultPath, 'utf-8');
-  expect(genDiff(fstPath, sndPath)).toBe(result);
+describe('json configs', () => {
+  test('empty configs', () => {
+    const fstPath = path.join(dirJson, 'empty.json');
+    const sndPath = fstPath;
+    const resultPath = path.join(dirJson, 'emptyDiff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
+
+  test('same', () => {
+    const fstPath = path.join(dirJson, 'fst.json');
+    const sndPath = path.join(dirJson, 'reversedFst.json');
+    const resultPath = path.join(dirJson, 'sameDiff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
+
+  test('diff of flat configs', () => {
+    const fstPath = path.join(dirJson, 'fst.json');
+    const sndPath = path.join(dirJson, 'snd.json');
+    const resultPath = path.join(dirJson, 'diff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
 });
 
-// test('fst empty, snd not empty', () => {});
-
-// test('fst not empty, snd empty', () => {});
-
-test('same', () => {
-  const fstPath = path.join(dirJson, 'fst.json');
-  const sndPath = path.join(dirJson, 'reversedFst.json');
-  const resultPath = path.join(dirJson, 'sameDiff.txt');
-  const result = fs.readFileSync(resultPath, 'utf-8');
-  expect(genDiff(fstPath, sndPath)).toBe(result);
-});
-
-test('diff of flat configs', () => {
-  const fstPath = path.join(dirJson, 'fst.json');
-  const sndPath = path.join(dirJson, 'snd.json');
-  const resultPath = path.join(dirJson, 'diff.txt');
-  const result = fs.readFileSync(resultPath, 'utf-8');
-  expect(genDiff(fstPath, sndPath)).toBe(result);
+describe('yaml configs', () => {
+  test('diff flat configs', () => {
+    const fstPath = path.join(dirYaml, 'fst.yaml');
+    const sndPath = path.join(dirYaml, 'snd.yaml');
+    const resultPath = path.join(dirYaml, 'diff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
 });
 
