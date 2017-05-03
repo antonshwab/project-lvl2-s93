a04e501d57d8a4aecee192419b64ad15a0e32e0c
Anton Shvab <shwab.ay@gmail.com>
2017-05-03 14:28:24 +0300

    rename

---
+++ src/parsers/index.js
@@ -0,0 +1,15 @@
+import json from './json';
+import ini from './ini';
+import yaml from './yaml';
+
+const parsers = { json, ini, yaml, yml: yaml };
+
+const getParser = (extname) => {
+  const parser = parsers[extname];
+  if (!parser) {
+    throw new Error(`${extname} extension is not supported!`);
+  }
+  return parser;
+};
+
+export default getParser;
