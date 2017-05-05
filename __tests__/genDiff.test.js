32e1b64ba162fcbca0c0cd277926b311e5b955c2
Anton Shvab <shwab.ay@gmail.com>
2017-05-05 21:54:37 +0300

    update fixtures and add nested test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,46 +5,54 @@ import genDiff from '../src/';
 const dirJson = '__tests__/__fixtures__/json/';
 const dirYaml = '__tests__/__fixtures__/yaml/';
 const dirIni = '__tests__/__fixtures__/ini/';
+const dirDiff = '__tests__/__fixtures__/diffs/';
 
 describe('json configs', () => {
   test('empty configs', () => {
     const fstPath = path.join(dirJson, 'empty.json');
     const sndPath = fstPath;
-    const resultPath = path.join(dirJson, 'emptyDiff.txt');
+    const resultPath = path.join(dirDiff, 'emptyDiff.txt');
     const result = fs.readFileSync(resultPath, 'utf-8');
     expect(genDiff(fstPath, sndPath)).toBe(result);
   });
 
   test('same', () => {
-    const fstPath = path.join(dirJson, 'fst.json');
+    const fstPath = path.join(dirJson, 'flatFst.json');
     const sndPath = path.join(dirJson, 'reversedFst.json');
-    const resultPath = path.join(dirJson, 'sameDiff.txt');
+    const resultPath = path.join(dirDiff, 'sameDiff.txt');
     const result = fs.readFileSync(resultPath, 'utf-8');
     expect(genDiff(fstPath, sndPath)).toBe(result);
   });
 
   test('diff flat configs', () => {
-    const fstPath = path.join(dirJson, 'fst.json');
-    const sndPath = path.join(dirJson, 'snd.json');
-    const resultPath = path.join(dirJson, 'diff.txt');
+    const fstPath = path.join(dirJson, 'flatFst.json');
+    const sndPath = path.join(dirJson, 'flatSnd.json');
+    const resultPath = path.join(dirDiff, 'flatDiff.txt');
     const result = fs.readFileSync(resultPath, 'utf-8');
     expect(genDiff(fstPath, sndPath)).toBe(result);
   });
 
-  // test('diff recursive configs', () => {
-  //   const fstPath = path.join(dirJson, 'recFst.json');
-  //   const sndPath = path.join(dirJson, 'recSnd.json');
-  //   const resultPath = path.join(dirJson, 'recDiff.txt');
-  //   const result = fs.readFileSync(resultPath, 'utf-8');
-  //   expect(genDiff(fstPath, sndPath)).toBe(result);
-  // });
+  test('diff nested configs', () => {
+    const fstPath = path.join(dirJson, 'nestedFst.json');
+    const sndPath = path.join(dirJson, 'nestedSnd.json');
+    const resultPath = path.join(dirDiff, 'nestedDiff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
 });
 
 describe('yaml configs', () => {
   test('diff flat configs', () => {
-    const fstPath = path.join(dirYaml, 'fst.yaml');
-    const sndPath = path.join(dirYaml, 'snd.yaml');
-    const resultPath = path.join(dirYaml, 'diff.txt');
+    const fstPath = path.join(dirYaml, 'flatFst.yaml');
+    const sndPath = path.join(dirYaml, 'flatSnd.yaml');
+    const resultPath = path.join(dirDiff, 'flatDiff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
+  test('diff nested configs', () => {
+    const fstPath = path.join(dirYaml, 'nestedFst.yaml');
+    const sndPath = path.join(dirYaml, 'nestedSnd.yaml');
+    const resultPath = path.join(dirDiff, 'nestedDiff.txt');
     const result = fs.readFileSync(resultPath, 'utf-8');
     expect(genDiff(fstPath, sndPath)).toBe(result);
   });
@@ -52,9 +60,16 @@ describe('yaml configs', () => {
 
 describe('ini configs', () => {
   test('diff flat configs', () => {
-    const fstPath = path.join(dirIni, 'fst.ini');
-    const sndPath = path.join(dirIni, 'snd.ini');
-    const resultPath = path.join(dirIni, 'diff.txt');
+    const fstPath = path.join(dirIni, 'flatFst.ini');
+    const sndPath = path.join(dirIni, 'flatSnd.ini');
+    const resultPath = path.join(dirDiff, 'flatDiff.txt');
+    const result = fs.readFileSync(resultPath, 'utf-8');
+    expect(genDiff(fstPath, sndPath)).toBe(result);
+  });
+  test('diff nested configs', () => {
+    const fstPath = path.join(dirIni, 'nestedFst.ini');
+    const sndPath = path.join(dirIni, 'nestedSnd.ini');
+    const resultPath = path.join(dirDiff, 'nestedDiff.txt');
     const result = fs.readFileSync(resultPath, 'utf-8');
     expect(genDiff(fstPath, sndPath)).toBe(result);
   });
