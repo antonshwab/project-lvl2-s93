e8076730dd2350864176631e2731da2059859991
Anton Shvab <shwab.ay@gmail.com>
2017-05-06 17:25:06 +0300

    add action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-
 import program from 'commander';
+import genDiff from '..';
 
 program
-  .version('0.0.1')
+  .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format (tree, plain)')
+  .action((firstConfig, secondConfig) =>
+    console.log(genDiff(firstConfig, secondConfig, program.format)))
   .parse(process.argv);
